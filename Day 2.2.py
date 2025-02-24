b3765e15695c6423572e62414d0268b92467e42d
Temmi556 <temmi556@mail.ru>
2025-02-24 23:08:43 +0300

    Total sum

--- Day 2.2.py
+++ Day 2.2.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
 adas = {
     "Products": ["Milk", "Bread", "Eggs", "Cheese"],
     "Price": [50, 30, 100, 80],
-    "Quantity": [2, 3, 1, 4],
+    "Quantity": [2 ,3 ,1 ,4 ],
     "Total": [100, 90, 100, 320]
 }
-print(pd.DataFrame(adas))
+bmk = pd.DataFrame(adas)
+print(bmk["Total"].sum())
+print(bmk)
